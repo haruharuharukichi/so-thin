a2db71d560bdd0fe40cc3550971c80f959431739
haruharuharukichi <pksdw031@gmail.com>
2019-02-18 11:38:45 +0900

    Updates

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
+//js一括読み込み
 jQuery(document).ready(function($) {
   $(function() {
-   $('#navbarToggler').on('click',function(){
-    $(this).toggleClass('active');
-  });
-   $('.scrollTop').on('click',function(){
-    $("html,body").animate({scrollTop:$('#header').offset().top});
-  });
-   $('.scrollBody').on('click',function(){
-    $("html,body").animate({scrollTop:$('#main').offset().top});
-  });
+    $('code').addClass('prettyprint');
+    $('pre').addClass('prettyprint');
+      /*
+    var nav = $('#globalNavi'),
+    offset = nav.offset();
+    $(window).on("scroll",function () {
+     if($(window).scrollTop() > offset.top) {
+       nav.addClass('fixed');
+     } else {
+       nav.removeClass('fixed');
+     }
+   });
+   */
+    $('#navbarToggler').on('click',function(){
+      $(this).toggleClass('active');
+    });
+    $('.scrollTop').on('click',function(){
+      $("html,body").animate({scrollTop:$('#header').offset().top});
+    });
+    $('.scrollBody').on('click',function(){
+      $("html,body").animate({scrollTop:$('#main').offset().top});
+    });
     //TOC
     $('#toc').toc({
       'selectors': 'main h2,main h3,main h4', //目次として表示する要素のCSSセレクターを指定
       'anchorName': function(i, heading, prefix) { //アンカーネームのカスタマイズ
-        return prefix+i;
+          return prefix+i;
       },
     });
      // フェードインしながら上へスライド    
-     $('.inviewfadeInUp').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    $('.inviewfadeInUp').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
       if (isInView) {
         $(this).addClass('fadeInUp');
       } else {
@@ -25,7 +39,7 @@ jQuery(document).ready(function($) {
       }
     });
      // フェードインしながら左へスライド    
-     $('.inviewfadeInLeft').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    $('.inviewfadeInLeft').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
       if (isInView) {
         $(this).addClass('fadeInLeft');
       } else {
